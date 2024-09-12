9977a0638953e3354e1d80e6149baeb82e31044e
Kevin Kalus <kevin.kalus@protofy.com>
2024-09-12 16:36:30 +0200

    removed modal button from shadcn and changed accordion behavior to only have one content open

--- src/components/Molecules/AccordionContent/AccordionContent.tsx
+++ src/components/Molecules/AccordionContent/AccordionContent.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { FC, ReactElement, useState } from 'react';
+import { FC, ReactElement } from 'react';
 
 interface AccordionContentProps {
   set: string;
@@ -9,18 +8,18 @@ interface AccordionContentProps {
     year: string;
     content: ReactElement;
   }[];
+  isOpen: boolean;
+  onToggle: () => void;
 }
 
-export const AccordionContent: FC<AccordionContentProps> = ({ children, set, name }) => {
-  const [expanded, setExpanded] = useState<boolean>(false);
+export const AccordionContent: FC<AccordionContentProps> = ({ children, set, name, isOpen, onToggle }) => {
   return (
     <div className="py-2">
       <h2>
         <button
           className="flex w-full items-center justify-between py-2 text-left font-semibold"
-          onClick={e => {
-            e.preventDefault();
-            setExpanded(!expanded);
+          onClick={() => {
+            onToggle();
           }}
         >
           <span className="flex w-full justify-between text-xs">
@@ -33,21 +32,21 @@ export const AccordionContent: FC<AccordionContentProps> = ({ children, set, nam
               width="16"
               height="2"
               rx="1"
-              className={`ttransform origin-center transition duration-200 ease-out ${expanded && '!rotate-180'}`}
+              className={`origin-center transform transition duration-200 ease-out ${isOpen && '!rotate-180'}`}
             />
             <rect
               y="7"
               width="16"
               height="2"
               rx="1"
-              className={`origin-center rotate-90 transform transition duration-200 ease-out ${expanded && '!rotate-180'}`}
+              className={`origin-center rotate-90 transform transition duration-200 ease-out ${isOpen && '!rotate-180'}`}
             />
           </svg>
         </button>
       </h2>
       <div
         role="region"
-        className={`grid overflow-hidden text-sm text-slate-600 transition-all duration-300 ease-in-out ${expanded ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
+        className={`grid overflow-hidden text-sm text-slate-600 transition-all duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
       >
         <div className="overflow-hidden">
           <div className="pb-3">{children}</div>
--- src/components/Organisms/Accordion/Accordion.tsx
+++ src/components/Organisms/Accordion/Accordion.tsx
@@ -1,5 +1,6 @@
+'use client';
 import clsx from 'clsx';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { mockSets } from '@/mocks/mockSets';
 
@@ -11,10 +12,20 @@ interface AccordionProps {
 }
 
 export const Accordion: FC<AccordionProps> = ({ className }) => {
+  const [expand, setExpand] = useState<number | false>(false);
+  const handleToggle = (index: number) => {
+    setExpand(prevIndex => (prevIndex === index ? false : index));
+  };
   return (
     <div className={clsx('divide-y divide-slate-200', className)}>
       {mockSets.map((set, index) => (
-        <AccordionContent key={index} set={set.set} name={set.name}>
+        <AccordionContent
+          key={index}
+          set={set.set}
+          name={set.name}
+          isOpen={expand === index}
+          onToggle={() => handleToggle(index)}
+        >
           <CardsGrid cards={set.cards} />
         </AccordionContent>
       ))}
