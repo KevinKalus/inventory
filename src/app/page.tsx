a2a5a90054535778fd61bf57d0a2962357bc6cec
Kevin Kalus <kevin.kalus@protofy.com>
2024-09-26 11:50:59 +0200

    fix: added app name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-20 text-center">
       <span>
-        This is the Hompage <br />
+        This is the Hompage of ForesTCG <br />
         Welcome Text follows soon
       </span>
     </div>
