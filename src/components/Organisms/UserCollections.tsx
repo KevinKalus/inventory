f72b090cd5a091ff2fc5d7af2278ed54b87b3a03
Kevin Kalus <kevin.kalus@protofy.com>
2024-09-30 10:18:23 +0200

    removed code

--- src/components/Organisms/UserCollections.tsx
+++ src/components/Organisms/UserCollections.tsx
@@ -1,8 +1,7 @@
 'use client'
 import { Collection, ImageFields } from "@/Types/Types"
 import { FC } from "react"
-import { CardsGrid } from "./CardsGrid/CardsGrid"
-import { mockCollection } from "@/mocks/mockCollections"
+
 
 interface UserCollectionsProps{
     collections:Collection[]
